c2e9c1b1a928a1afd042701439601ff3f53a4b1c
xXhariboXx <haribo.raczka@gmail.com>
2019-02-19 00:44:14 +0100

    [MISC] Little changes to interfaces

--- PUM.MobileApp/ViewModels/Interfaces/BackableViewModel.cs
+++ PUM.MobileApp/ViewModels/Interfaces/BackableViewModel.cs
@@ -1,14 +1,15 @@
-﻿using PUM.MobileApp.Commands;
+﻿using GalaSoft.MvvmLight;
+using PUM.MobileApp.Commands;
 using System.Windows.Input;
 
 namespace PUM.MobileApp.ViewModels.Interfaces
 {
-    public interface IBackableViewModel
+    public interface IBackableViewModel : IAppBarableViewModel
     {
         ICommand BackToMainMenuCommand { get; }
     }
 
-    public class BackableViewModel : IBackableViewModel
+    public class BackableViewModel : ViewModelBase, IBackableViewModel
     {
         private ICommand backToMainMenuCommand;
 
@@ -20,6 +21,20 @@ namespace PUM.MobileApp.ViewModels.Interfaces
                     backToMainMenuCommand = new NavigationCommand("MainMenu");
 
                 return backToMainMenuCommand;
+            }
+        }
+
+        private string currentView;
+        public string CurrentView
+        {
+            get
+            {
+                return currentView;
+            }
+            set
+            {
+                currentView = value;
+                RaisePropertyChanged("CurrentView");
             }
         }
     }
